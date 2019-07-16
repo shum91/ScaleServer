ab89529a16e21842f3f3a2fa3ca40df3886888dd
shum91 <savik@spy.im>
2019-07-16 15:53:59 +0300

    UP

--- SHTRIH-Com/RUN.bat
+++ SHTRIH-Com/RUN.bat
@@ -1,3 +1,8 @@
 :: This example run script
-SHTRIH-COM.exe "0.0.0.0::50505::COM7::9600::30" 1>> "%Date:~6,4%%Date:~3,2%%Date:~0,2%_out.log" 2>>&1
+
+shtrih-com.exe ":50505:COM1:9600" 1>> "%Date:~6,4%%Date:~3,2%%Date:~0,2%_out.log" 2>>&1
+
+:: not log
+:: shtrih-com.exe ":50505:COM1:9600" 1>> NUL 2>>&1
+
 pause
